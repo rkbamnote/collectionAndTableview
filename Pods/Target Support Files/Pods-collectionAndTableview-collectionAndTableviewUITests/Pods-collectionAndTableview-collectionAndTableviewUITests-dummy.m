060550cb78271e4be90f2e7b33cfa861d09f203d
rkbamnote <104487404+rkbamnote@users.noreply.github.com>
2022-07-19 20:07:29 +0530

    Add files via upload

---
+++ Pods/Target Support Files/Pods-collectionAndTableview-collectionAndTableviewUITests/Pods-collectionAndTableview-collectionAndTableviewUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_collectionAndTableview_collectionAndTableviewUITests : NSObject
+@end
+@implementation PodsDummy_Pods_collectionAndTableview_collectionAndTableviewUITests
+@end
